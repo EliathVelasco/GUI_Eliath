175a303d83f9e8bb652813bddde2dd9e04ae3286
Eliath <eliath.velasco@epn.edu.ec>
2022-08-25 20:47:32 -0500

    eeeeee

--- src/ajedrez/partida/Partida.java
+++ src/ajedrez/partida/Partida.java
@@ -2,13 +2,21 @@ package ajedrez.partida;
 
 import ajedrez.excepciones.*;
 
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Partida {
     private final Tablero tablero;
+
     private ColorPiezas colorTurnoActual;
+
+
     private Jugador jugadorBlanco;
     private Jugador jugadorNegro;
+
+
 
     public Partida() {
         jugadorBlanco = new Jugador(ColorPiezas.BLANCAS);
@@ -17,48 +25,31 @@ public class Partida {
         tablero = new Tablero();
     }
 
-    public Movimiento preguntarMovimiento(Jugador jugador) throws SintaxisInvalida {
-        Scanner scannerDelJugador = new Scanner(System.in);
-        String jugada = scannerDelJugador.next();
-        return new Movimiento(jugada, jugador);
-
-    }
-
-    /*
-     * hacerMovimiento(){
-     *   if(validarMovimiento()){****}
-     * }
-     *
-     * private boolean validarMovimiento(Moviento){
-     *
-     * }
-     * */
     public void jugarPartida() {
         for (; ; ) {
             while (colorTurnoActual == ColorPiezas.BLANCAS) {
-                try {
-                    tablero.hacerMovimiento(preguntarMovimiento(jugadorBlanco));
-                    System.out.println(tablero);
-                    colorTurnoActual = ColorPiezas.NEGRAS;
-                }catch(SintaxisInvalida si){
-                    System.out.println(si.getMessage());
-                }catch(MovimientoInvalido mi){
-                    System.out.println(mi.getMessage());
-                }
+                colorTurnoActual = ColorPiezas.NEGRAS;
             }
-
             while (colorTurnoActual == ColorPiezas.NEGRAS) {
-                try {
-                    tablero.hacerMovimiento(preguntarMovimiento(jugadorNegro));
-                    System.out.println(tablero);
-                    colorTurnoActual = ColorPiezas.BLANCAS;
-                }catch(SintaxisInvalida si){
-                    System.out.println(si.getMessage());
-                }catch(MovimientoInvalido mi){
-                    System.out.println(mi.getMessage());
-                }
+                colorTurnoActual = ColorPiezas.BLANCAS;
             }
         }
+    }
+
+    public ColorPiezas getColorTurnoActual() {
+        return colorTurnoActual;
+    }
+
+    public Jugador getJugadorBlanco() {
+        return jugadorBlanco;
+    }
+
+    public Tablero getTablero() {
+        return tablero;
+    }
+
+    public Jugador getJugadorNegro() {
+        return jugadorNegro;
     }
 
     public void imprimirTablero() {
